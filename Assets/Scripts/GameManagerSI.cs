0024788323f50094bf1af30a24943224804416c2
Rob <az5rob@gmail.com>
2014-09-06 16:00:10 +0100

    done the invader spawn

---
+++ Assets/Scripts/GameManagerSI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerSI : MonoBehaviour
+{
+
+    // All of the Managers and public scripts.
+    public EnemyManagerSI enemyManagerSI;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
