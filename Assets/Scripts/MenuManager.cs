874ad009100e054c3ba4ec8a8d9c00f94c3ff060
Rob <az5rob@gmail.com>
2014-09-20 20:08:56 +0100

    Menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public List<GameObject> menuObjects = new List<GameObject>();
+
+    public void ChangeScene(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+    public void Settings()
+    {
+        foreach(GameObject mo in menuObjects)
+        {
+            if(mo)
+            {
+                mo.SetActive(!mo.activeSelf);
+            }
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
+
